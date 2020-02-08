46d3637cd814fd368ce6b17a6c2d47913a1189cd
shelby palumbo <shelbypalumbo94@gmail.com>
2020-02-08 12:34:40 -0500

    additions to script

--- reserve.js
+++ reserve.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var reserve = [
+var newReservation = [
     {
     routeName:"reserve",
     name: "sdfg",
@@ -59,17 +59,17 @@ app.get("/api/tables", function (req, res) {
 app.post("/api/tables", function (req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
-    var newReservation = req.body;
+    var newReservation2 = req.body;
 
     // Using a RegEx Pattern to remove spaces from newCharacter
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase(); //replace(/\s+/g, "") is getting rid of spaces
+    newReservation2.routeName = newReservation2.name.replace(/\s+/g, "").toLowerCase(); //replace(/\s+/g, "") is getting rid of spaces
 
-    console.log(newReservation);
+    console.log(newReservation2);
 
-    reserve.push(newReservation);
+    reserve.push(newReservation2);
 
-    res.json(newReservation);
+    res.json(newReservation2);
 });
 
 
