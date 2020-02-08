5f2271d3cc6f0d8ab6f4df34f2aaf5913c1ba7a2
shelby palumbo <shelbypalumbo94@gmail.com>
2020-02-08 11:45:51 -0500

    removed incorrect home.html

--- reserve.js
+++ reserve.js
@@ -38,7 +38,7 @@ var waiting = [
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "reserve.html"));
+    res.sendFile(path.join(__dirname, "home.html"));
 });
 
 app.get("/reserve", function (req, res) { //add a character
