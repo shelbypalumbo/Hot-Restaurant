5217320dc66c1340290f2fd2a3918cb42215dcca
shelby palumbo <shelbypalumbo94@gmail.com>
2020-02-09 22:44:21 -0500

    Additions to tables.html, removed/renames files in script and reserve.html

--- reserve.js
+++ reserve.js
@@ -1,10 +1,7 @@
-// Dependencies
-// =============================================================
 var express = require("express");
 var path = require("path");
 
 // Sets up the Express App
-// =============================================================
 var app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -13,30 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 var newReservation = [
-    {
-    routeName:"reserve",
-    name: "sdfg",
-    phone: "sdfg",
-    email: "sdf",
-    id: "",
-    party: ""
-}
 ];
 
 var waiting = [
-    {   routeName:"tables",
-        name: "",
-        phone: "",
-        email: "",
-        id: "",
-        party: ""
-}
 ];
 
-// Routes
-// =============================================================
 
-// Basic route that sends the user first to the AJAX Page
+//Routes
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
 });
@@ -45,50 +25,32 @@ app.get("/reserve", function (req, res) { //add a character
     res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
-app.get("/tables", function (req, res) { 
+app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
 });
 
-// Displays all tables
+// Displays all tables and waitlist
 app.get("/api/tables", function (req, res) {
-    return res.json(newReservation);
+    res.json(newReservation);
 });
 
 app.get("/api/waitlist", function (req, res) {
-    return res.json(newReservation);
+    res.json(newReservation);
 });
 
 
 
 app.post("/api/tables", function (req, res) {
-    
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    var newReservation2 = req.body;
 
-    console.log(newReservation2);
-
-    newReservation.push(newReservation2);
-
-    res.json(newReservation2);
-
-    if(newReservation.length < 5){
-
-app.post("/api/waitlist", function (req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-  
-    var newReservation2 = req.body;
-
-    console.log(newReservation2);
-
-    waiting.push(newReservation2);
-
-    res.json(newReservation2);
-});}
-console.log(newReservation2.length);
+    if (newReservation.length < 5) {
+        newReservation.push(req.body);
+        res.json(true);
+    }
+    else {
+        waiting.push(req.body);
+        res.json(false);
+    }
 });
-
 
 
 // Starts the server to begin listening
